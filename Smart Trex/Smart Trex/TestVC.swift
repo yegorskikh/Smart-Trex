770dd504cc9356b8e5021bcf06391035c98139e0
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-01-25 20:50:02 +0300

    feat: architecture setup

---
+++ Smart Trex/Smart Trex/TestVC.swift
@@ -0,0 +1,19 @@
+//
+//  TestVC.swift
+//  Smart Trex
+//
+//  Created by Yegor Gorskikh on 25.01.2022.
+//
+
+import UIKit
+
+class TestVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
