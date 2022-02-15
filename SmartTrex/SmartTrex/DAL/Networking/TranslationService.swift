ba77c529e43add67f9c88e7941e158741be2e969
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-02-15 13:41:11 +0300

    feat: impl service for translate

--- SmartTrex/SmartTrex/DAL/Networking/TranslationService.swift
+++ SmartTrex/SmartTrex/DAL/Networking/TranslationService.swift
@@ -1,6 +1,74 @@
 import Foundation
 import Alamofire
 
+struct WordRequestModel: Encodable {
+    let q: String
+    let target: String
+}
+
+
+
+struct ResponeData: Decodable {
+    let data: TranslationsResponseModel?
+}
+
+struct TranslationsResponseModel: Decodable {
+    let translations: [WordResponseModel]?
+}
+
+struct WordResponseModel: Decodable {
+    let translatedText: String?
+}
+
+
 class TranslationService {
     
+    let session: Session = Session(configuration: URLSessionConfiguration.default)
+    let url = URL(string: "https://google-translate1.p.rapidapi.com/language/translate/v2")!
+    
+    let headers: HTTPHeaders = [
+        "content-type": "application/x-www-form-urlencoded",
+        "accept-encoding": "application/gzip",
+        "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+        "x-rapidapi-key": "9d2d2a03c1mshc9f23a48517a991p1dd382jsna54af8d5d5bd"
+    ]
+    
+    func dontWork(_ word: WordRequestModel, completion: @escaping ((ResponeData?) -> Void)) {
+        
+        session.request(url,
+                        method: .post,
+                        parameters: WordRequestModel(q: "Hello", target: "es"),
+                        encoder: URLEncodedFormParameterEncoder.default,
+                        headers: headers)
+            .response { response in
+
+                guard
+                    let responseData = response.data,
+                    let statusCode = response.response?.statusCode
+                else {
+                    print("Faild response data")
+                    completion(nil)
+                    return
+                }
+
+                print("Status code: ", statusCode)
+                
+                do {
+                    let data = try JSONDecoder().decode(ResponeData.self, from: responseData)
+                    print("Success! - ", data)
+                    completion(data)
+                }
+                catch {
+                    print("Faild decode data")
+                    completion(nil)
+                }
+            }
+            
+    }
+    
+    
 }
+
+
+
+
--- SmartTrex/SmartTrex/TestVC.swift
+++ SmartTrex/SmartTrex/TestVC.swift
@@ -13,9 +13,14 @@ class TestVC: UIViewController {
     @IBOutlet weak var originalLabel: UILabel!
     @IBOutlet weak var translationLabel: UILabel!
     
+    let service = TranslationService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        service.dontWork(.init(q: "hi", target: "en")) { data in
+        //    print(data)
+        }
+      //  printTest()
     }
     
     
@@ -35,8 +40,7 @@ class TestVC: UIViewController {
 //            "x-rapidapi-key": "9d2d2a03c1mshc9f23a48517a991p1dd382jsna54af8d5d5bd"
 //        ]
 //
-//        let postData = NSMutableData(data: "q=Hello, world!&target=ru&source=en".data(using: String.Encoding.utf8)!)
-//
+//        let postData = NSMutableData(data: "q=Hello&target=ru&source=en".data(using: String.Encoding.utf8)!)
 //        let request = NSMutableURLRequest(url: NSURL(string: "https://google-translate1.p.rapidapi.com/language/translate/v2")! as URL,
 //                                                cachePolicy: .useProtocolCachePolicy,
 //                                            timeoutInterval: 10.0)
@@ -45,20 +49,15 @@ class TestVC: UIViewController {
 //        request.httpBody = postData as Data
 //
 //        let session = URLSession.shared
-//        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-//            if (error != nil) {
-//                print(error)
-//            } else {
-//                let httpResponse = response as? HTTPURLResponse
-//                print(httpResponse)
-//                print("This is translate -", data)
+//        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, _, _) -> Void in
 //                do {
-//                    let transData = try JSONDecoder.decode(String.self, from )
+//                    let transData = try JSONDecoder().decode(TranslationsResponsePayload.self, from: data!)
+//                    print("This is translate - transData", transData)
 //                }
 //                catch let error {
 //                    print(error.localizedDescription)
 //                }
-//            }
+//            
 //        })
 //
 //        dataTask.resume()
@@ -66,6 +65,3 @@ class TestVC: UIViewController {
     
 }
 
-struct Tr: Codable {
-    
-}
