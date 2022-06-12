69ccf07c7ba28c2e50668563ff14f6eebf5665cb
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-06-12 18:40:13 +0300

    ST17-feat: impl translate rx viewmodel tests

--- SmartTrex/SmartTrexTests/PresentationTests/TranslateViewModelTests.swift
+++ SmartTrex/SmartTrexTests/PresentationTests/TranslateViewModelTests.swift
@@ -7,50 +7,130 @@
 
 import Foundation
 import XCTest
+import RxTest
+import RxBlocking
+import RxSwift
 @testable import SmartTrex
 
 class TranslateViewModelTests: XCTestCase {
     
     var sut: TranslateViewModel!
     var interactorMock: TranslateInteractorMock!
-    var viewMock: TranslateVcMock!
+    
+    private var scheduler: TestScheduler!
+    private var disposeBag: DisposeBag!
     
     override func setUp() {
         super.setUp()
         interactorMock = TranslateInteractorMock()
-
-        sut = TranslateViewModel()
-        sut.interactor = interactorMock
+        sut = TranslateViewModel(
+            interactor: interactorMock
+        )
+        
+        scheduler = TestScheduler(initialClock: 0)
+        disposeBag = DisposeBag()
     }
     
     override func tearDown() {
         sut = nil
         interactorMock = nil
+        scheduler = nil
+        disposeBag = nil
         super.tearDown()
     }
     
     func test_success_translate() {
-        //        // given
-        //        interactorMock.typeResponse = .succes
-        //
-        //        // when
-        //        sut.translate()
-        //
-        //        // then
-        //        XCTAssertEqual(interactorMock.wasCalled, true)
-        //        XCTAssertEqual(viewMock.setTranslationTextWasCalled, true)
+        // given
+        let tappedToTranslate = scheduler.createObserver(Void.self)
+        let textToTranslate = scheduler.createObserver(String.self)
+        let targetToTranslate = scheduler.createObserver(String.self)
+        
+        sut.sendAction
+            .subscribe(tappedToTranslate)
+            .disposed(by: disposeBag)
+        
+        sut.textToTranslate
+            .subscribe(textToTranslate)
+            .disposed(by: disposeBag)
+        
+        sut.targetToTranslate
+            .subscribe(targetToTranslate)
+            .disposed(by: disposeBag)
+        
+        // when
+        scheduler.createColdObservable([.next(10, ())])
+            .bind(to: sut.input.onSendAction)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(12, "Foo")])
+            .bind(to: sut.input.onToTranslate)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(12, "Bar")])
+            .bind(to: sut.input.onTarget)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        // then
+        XCTAssertEqual(textToTranslate.events, [
+            .next(0, ""),
+            .next(12, "Foo")
+        ])
+        XCTAssertEqual(targetToTranslate.events, [
+            .next(0, ""),
+            .next(12, "Bar")
+        ])
+        XCTAssertEqual(interactorMock.wasCalled, true)
     }
     
-    func test_faild_translate() {
-        //        // given
-        //        interactorMock.typeResponse = .error
-        //
-        //        // when
-        //        sut.translate()
-        //
-        //        // then
-        //        XCTAssertEqual(interactorMock.wasCalled, true)
-        //        XCTAssertEqual(viewMock.showAlertWasCalled, true)
+    func test_failed_translate() {
+        // given
+        interactorMock.typeResponse = .error
+        
+        let tappedToTranslate = scheduler.createObserver(Void.self)
+        let textToTranslate = scheduler.createObserver(String.self)
+        let targetToTranslate = scheduler.createObserver(String.self)
+        
+        sut.sendAction
+            .subscribe(tappedToTranslate)
+            .disposed(by: disposeBag)
+        
+        sut.textToTranslate
+            .subscribe(textToTranslate)
+            .disposed(by: disposeBag)
+
+        sut.targetToTranslate
+            .subscribe(targetToTranslate)
+            .disposed(by: disposeBag)
+        
+        // when
+        scheduler.createColdObservable([.next(10, ())])
+            .bind(to: sut.input.onSendAction)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.completed(11)])
+            .bind(to: sut.input.onToTranslate)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.completed(11)])
+            .bind(to: sut.input.onTarget)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        // then
+        XCTAssertEqual(textToTranslate.events, [
+            .next(0, ""),
+            .completed(11)
+        ])
+        
+        XCTAssertEqual(targetToTranslate.events, [
+            .next(0, ""),
+            .completed(11) //
+        ])
+        
+        XCTAssertEqual(interactorMock.wasCalled, true)
     }
     
 }
